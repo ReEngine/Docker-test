39dbfb0bd07c3798d8f3c7e08b1a10b7767a883a
Игорь Амелин <amelin.it@gmail.com>
2024-03-31 22:09:47 +0300

    initial commit

---
+++ src/public/assets/js/image.js
@@ -0,0 +1,16 @@
+function displayImage(src,picnum) {
+    var imgBox = document.createElement("div");
+    var img = document.createElement("img");
+    imgBox.className = "imgbox";
+    img.src = src+picnum+".jpg";
+    img.className = "image";
+    var imgblur = img.cloneNode();
+    imgBox.appendChild(img);
+    imgblur.className = "blur";
+    imgBox.appendChild(imgblur);
+    document.body.appendChild(imgBox);
+   }
+
+   var rand = parseInt(Math.random()*5);
+   
+displayImage("./public/assets/images/",rand);
